a8629b2e4cc0b35c91379760686e206a82660184
Семен <marbledrop@yandex.ru>
2025-06-15 23:56:31 +0300

    Add Example003 Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите первое число:");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int NumberB = Convert.ToInt32(Console.ReadLine());
+int Sum = NumberA + NumberB;
+Console.WriteLine(Sum);
