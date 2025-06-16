8f01415e5663e03221a9f960a74b3e3c2fc4807b
Семен <marbledrop@yandex.ru>
2025-06-16 21:39:56 +0300

    Add Exaple 006 Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+int A = 1;
+int B = 8;
+int C = 3;
+int D = 2;
+int E = 6;
+
+int Max = A;
+
+if (A > Max) Max = A;
+if (B > Max) Max = B;
+if (C > Max) Max = C;
+if (D > Max) Max = D;
+if (E > Max) Max = E;
+
+Console.Write("Max = ");
+Console.WriteLine(Max);
