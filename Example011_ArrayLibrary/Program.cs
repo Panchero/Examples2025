19f1c7f0d2dfa7ff2702c4bd00e99ab314ee0d2a
Панченко <panchenko@inside.vologda-portal.ru>
2025-06-20 15:20:33 +0300

    Add Example011_ArrayLibrary

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,18 @@
+﻿void FillArray (int[] collection)
+{
+    int Length = collection.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+
+
+
+
+
+int[] array = new int[10]; // создай новый массив, в котором будет 10 элементов
+
