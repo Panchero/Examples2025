499206517dc7988d01a6537edd6239b859561158
Семен <marbledrop@yandex.ru>
2025-07-25 00:22:18 +0300

    Add Example009_IntroArray++

--- Example009_IntroArray/Program.cs
+++ Example009_IntroArray/Program.cs
@@ -9,3 +9,4 @@ int[] array = { 11, 21, 31, 41, 15, 61, 17, 18, 19 };
 int result = Max(Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]), Max(array[6], array[7], array[8]));
 
 Console.WriteLine(result);
+//++
